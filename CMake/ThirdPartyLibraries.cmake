6a5357f48fb9323193be307323797db2ddc313cb
Kevin Scroggins <nitro404@gmail.com>
2023-04-20 19:55:15 -0400

    Remove legacy command line interface code
    - Remove GUI_ENABLED CMake option since using a graphical user interface is now the only option.

--- CMake/ThirdPartyLibraries.cmake
+++ CMake/ThirdPartyLibraries.cmake
@@ -1,22 +1,20 @@
 include_guard()
 
-if(GUI_ENABLED)
-	# wxWidgets Dependencies
-	hunter_add_package(EXPAT)
-	hunter_add_package(JPEG)
-	hunter_add_package(nanosvg)
-	hunter_add_package(PCRE2)
-	hunter_add_package(png)
+# wxWidgets Dependencies
+hunter_add_package(EXPAT)
+hunter_add_package(JPEG)
+hunter_add_package(nanosvg)
+hunter_add_package(PCRE2)
+hunter_add_package(png)
 
-	find_package(EXPAT CONFIG REQUIRED)
-	find_package(JPEG CONFIG REQUIRED)
-	find_package(nanosvg CONFIG REQUIRED)
-	find_package(PCRE2 CONFIG REQUIRED)
-	find_package(png CONFIG REQUIRED)
-	find_package(OpenGL)
+find_package(EXPAT CONFIG REQUIRED)
+find_package(JPEG CONFIG REQUIRED)
+find_package(nanosvg CONFIG REQUIRED)
+find_package(PCRE2 CONFIG REQUIRED)
+find_package(png CONFIG REQUIRED)
+find_package(OpenGL)
 
-	# Application Dependencies
-	hunter_add_package(wxWidgets)
+# Application Dependencies
+hunter_add_package(wxWidgets)
 
-	find_package(wxWidgets CONFIG REQUIRED)
-endif()
+find_package(wxWidgets CONFIG REQUIRED)
